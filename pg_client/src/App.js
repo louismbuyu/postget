158182aaaa55804bf32eb9c910983358d36d0e20
Louis Mbuyu <louismbuyu17@gmail.com>
2019-02-15 23:31:04 -0600

    updated ui design

--- pg_client/src/App.js
+++ pg_client/src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Nav from "./components/nav/Nav";
+import Post from "./components/post/Post";
+import {Col, Row} from "antd";
+import Get from "./components/get/Get";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Nav/>
+          <div className="App">
+              <Row gutter={16}>
+                  <Col span={12}>
+                      <Post/>
+                  </Col>
+                  <Col span={12}>
+                      <Get/>
+                  </Col>
+              </Row>
+          </div>
       </div>
     );
   }
